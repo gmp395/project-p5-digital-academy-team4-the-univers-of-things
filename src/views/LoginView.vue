c619ca0162adb3a97dbd566bf1514e6258c85bd4
Raana <raanaafkari96@gmail.com>
2026-06-15 12:06:26 +0200

    feat: setup layout for LoginView

---
+++ src/views/LoginView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login-container">
+    <section class="login-brand">
+      <div class="brand-content">
+        <img src="@/assets/logo.png" alt="CineMagic Logo" />
+        <h1>CineMagic</h1>
+        <p>Inicia sesión para descubrir la magia de cada personaje y gestionar tus favoritos.</p>
+      </div>
+    </section>
+
+    <section class="login-form-side">
+      <LoginForm />
+    </section>
+  </div>
+</template>
+
+<script setup>
+import LoginForm from '@/components/LoginForm.vue';
+</script>
+
+<style scoped>
+/* Stiller bir sonraki adımda eklenecek */
+</style>
