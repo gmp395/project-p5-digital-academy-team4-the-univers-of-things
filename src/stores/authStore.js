1cd1e72e4281a9a6c549472c457c51d877bf02cd
Raana <raanaafkari96@gmail.com>
2026-06-17 12:46:55 +0200

    feat: add auth store state and logout action

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,3 +1,16 @@
 import { defineStore } from 'pinia'
 
-export const useAuthStore = defineStore('auth', {})
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null,
+    token: null,
+    isAuthenticated: false
+  }),
+
+ actions: {
+  logout() {
+    this.user = null
+    this.token = null
+    this.isAuthenticated = false
+  }
+}
