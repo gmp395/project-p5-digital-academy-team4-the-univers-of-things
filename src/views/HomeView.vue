7bf37e733b795ecff7fc9481062c488fc55b0e7e
andreaperezgon <andreaperezgon@gmail.com>
2026-06-17 10:17:29 +0200

    renderiza multiples tartejas con v-for

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,32 @@
 <script setup>
 import TarjetasPersonajes from '../componentes/tarjetaspersonajes.vue'
 
-const personajePrueba = {
-  name: 'Baloo',
-  imageUrl: 'https://static.wikia.nocookie.net/disney/images/0/03/Profile_-_Baloo.jpeg'
-}
+const personajes = [
+  {
+    id: 1,
+    name: 'Baloo',
+    imageUrl: 'https://static.wikia.nocookie.net/disney/images/0/03/Profile_-_Baloo.jpeg'
+  },
+  {
+    id: 2,
+    name: 'Mickey Mouse',
+    imageUrl: 'https://picsum.photos/300/400?1'
+  },
+  {
+    id: 3,
+    name: 'Donald Duck',
+    imageUrl: 'https://picsum.photos/300/400?2'
+  }
+]
 </script>
 
 <template>
   <main>
     <h1>Disney Characters</h1>
 
-    <TarjetasPersonajes :personaje="personajePrueba" />
-  </main>
+    <TarjetasPersonajes :personaje="personajePrueba" /><TarjetasPersonajes
+  v-for="personaje in personajes"
+  :key="personaje.id"
+  :personaje="personaje"
+/>  </main>
 </template>
