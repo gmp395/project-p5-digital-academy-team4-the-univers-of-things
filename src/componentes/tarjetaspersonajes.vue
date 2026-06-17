d5b57e6d5b562a55e2aaeeda32b468bd7fd0de0f
andreaperezgon <andreaperezgon@gmail.com>
2026-06-17 10:06:34 +0200

    configura props del componente

--- src/componentes/tarjetaspersonajes.vue
+++ src/componentes/tarjetaspersonajes.vue
@@ -1,19 +1,9 @@
 <template>
-  <article class="overflow-hidden rounded-2xl bg-slate-900 shadow-lg transition hover:-translate-y-1 hover:shadow-2xl">
-    <img
-      :src="personaje.imageUrl"
-      :alt="personaje.name"
-      class="h-60 w-full object-cover"
-    />
-
-    <div class="p-4">
-      <h3 class="text-center text-lg font-semibold text-white">
-        {{ personaje.name }}
-      </h3>
-    </div>
+  <article>
+    <img src="" alt="personaje">
+    <h3>Nombre personaje</h3>
   </article>
 </template>
-
 <script setup>
 defineProps({
   personaje: {
