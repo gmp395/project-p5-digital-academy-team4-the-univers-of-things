6a01137c85c6807f5babfd0987ea4d80b00f2fef
andreaperezgon <andreaperezgon@gmail.com>
2026-06-18 10:00:20 +0200

    fix: fixed router. refactored hireachy components cards

--- src/router/index.js
+++ src/router/index.js
@@ -8,16 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue')
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/RegisterView.vue')
     }
   ]
 })
