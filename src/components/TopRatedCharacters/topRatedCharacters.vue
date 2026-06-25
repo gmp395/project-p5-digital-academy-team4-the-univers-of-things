9373d008c01cee48cf3312616a0f3ea6a4c3b2d8
andreaperezgon <andreaperezgon@gmail.com>
2026-06-25 10:23:19 +0200

    archivo 5 mejores valorados y estructura base creada

---
+++ src/components/TopRatedCharacters/topRatedCharacters.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const topRatedCharacters = ref([])
+</script>
+
+<template>
+  <section class="px-6 py-10">
+    <h2 class="mb-6 text-2xl font-bold text-white">
+      ⭐ Top Rated by Users
+    </h2>
+  </section>
+</template>
