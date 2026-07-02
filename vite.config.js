b4123bb5939ecf500aa8c2c993035d1abcf4b5b4
Luisa Cortes <luisa-1609@hotmail.com>
2026-07-02 12:49:44 +0200

    ci: add GitHub Pages deploy workflow and base URL

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/project-p5-digital-academy-team4-the-univers-of-things/',
   plugins: [
     vue(),
     vueDevTools(),
